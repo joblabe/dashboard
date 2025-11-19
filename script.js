5a294efbdb93cee5c26e8b1c88f1dba21f6da144
joblabe <joblabeoc@gmail.com>
2025-11-19 11:46:28 -0500

    esructra item 1

---
+++ script.js
@@ -0,0 +1,104 @@
+const PUERTO_CARREÑO_DATA = {
+    
+    "poblacion_total_escalada": 1000,
+    
+    "sexo": [
+        {
+            "label": "Femenino",
+            "count": 473
+        },
+        {
+            "label": "Masculino",
+            "count": 527
+        }
+    ],
+    
+    "rango_edad": [
+        {
+            "label": "< 12 años",
+            "count": 248
+        },
+        {
+            "label": "12 a 17 años",
+            "count": 119
+        },
+        {
+            "label": "18 a 24 años",
+            "count": 137
+        },
+        {
+            "label": "25 a 29 años",
+            "count": 88
+        },
+        {
+            "label": "30 a 34 años",
+            "count": 81
+        },
+        {
+            "label": "35 a 39 años",
+            "count": 74
+        },
+        {
+            "label": "40 a 44 años",
+            "count": 63
+        },
+        {
+            "label": "45 a 49 años",
+            "count": 48
+        },
+        {
+            "label": "50 a 54 años",
+            "count": 41
+        },
+        {
+            "label": "55 a 59 años",
+            "count": 34
+        },
+        {
+            "label": "60 a 64 años",
+            "count": 27
+        },
+        {
+            "label": "65 años o más",
+            "count": 40
+        }
+    ],
+    
+    "etnia": [
+        {
+            "label": "No Indígena",
+            "count": 762
+        },
+        {
+            "label": "Sikuani",
+            "count": 131
+        },
+        {
+            "label": "Curripaco",
+            "count": 71
+        },
+        {
+            "label": "Piaroa",
+            "count": 36
+        }
+    ],
+    
+    "dialecto": [
+        {
+            "label": "Español (Principal)",
+            "count": 762
+        },
+        {
+            "label": "Sikuani (Lengua Nativa)",
+            "count": 131
+        },
+        {
+            "label": "Curripaco (Lengua Nativa)",
+            "count": 71
+        },
+        {
+            "label": "Piaroa (Lengua Nativa)",
+            "count": 36
+        }
+    ]
+};
